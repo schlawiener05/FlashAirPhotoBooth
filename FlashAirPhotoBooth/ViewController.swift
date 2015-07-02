4e338f677b726786e5dc3eb1dac2b35c6bc2db48
schlawiener <schla-wiener@gmx.net>
2015-07-02 17:49:52 +0200

    Initial Commit

---
+++ FlashAirPhotoBooth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlashAirPhotoBooth
+//
+//  Created by Janosch Achenbach on 7/2/15.
+//  Copyright (c) 2015 Janosch Achenbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
